66b67eea237ec0ce90cdd6a52bce007b1bdcc405
PabloXantini <santinypabloe130@gmail.com>
2025-08-10 03:13:09 -0600

    docs: old implementation removed

--- src/EPGame/ExagonPlusApp.cpp
+++ src/EPGame/ExagonPlusApp.cpp
@@ -11,17 +11,4 @@ void ExagonPlus::execute(){
     Game game(&engine);
     game.init();
     game.run();
-    //Window setup
-    //auto mainWindow = engine.getGraphics().getWinManager()->createWindow(800, 600, "ExagonPlus");
-    //engine.getGraphics().getWinManager()->createWindow(1200, 800, "ExagonPlus Ventana Secundaria");
-
-    //Scene Setup
-    //RenderTest test(&engine);
-    //test.init();
-
-    //mainWindow->setScene(&test);
-
-    //ULTIMO METODO QUE VOY A EJECUTAR
-    //engine.getGraphics().getWinManager()->runAsOnlyWindow(mainWindow);
-    //engine.getGraphics().getWinManager()->runAsVariousWindows(mainWindow);
 }
