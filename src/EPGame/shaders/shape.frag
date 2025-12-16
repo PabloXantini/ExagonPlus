2eaaa05a85b4af9aab1cc82b198e938ff33cce84
PabloXantini <santinypabloe130@gmail.com>
2025-12-15 21:57:58 -0600

    BREAKING CHANGE: Indevelopment engine migrated in another project

--- src/EPGame/shaders/shape.frag
+++
@@ -1,8 +0,0 @@
-#version 330 core
-
-in vec4 oFragColor;
-out vec4 FragColor;
-
-void main() {
-    FragColor = vec4(oFragColor);
-}
--- src/PEngine/PlatformCore/PlatformCore.cpp
+++
@@ -1,7 +0,0 @@
-#include "PlatformCore.hpp"
-
-bool PlatformCore::init(std::string platform_name){
-    if(platform_name=="desktop"){
-        pImplementation = new CoreDesktopGLFW();
-    }      
-}
